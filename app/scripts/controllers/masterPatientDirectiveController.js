ba7d6091a184eda68c6aeeef6199ce7aaf6478e6
Martin Scholl <martin.scholl@cismet.de>
2013-11-28 17:59:33 +0100

    [issue #2] added sorting and filtering

--- app/scripts/controllers/masterPatientDirectiveController.js
+++ app/scripts/controllers/masterPatientDirectiveController.js
@@ -3,13 +3,12 @@ angular.module('eu.crismaproject.pilotE.controllers')
         [
             '$scope',
             '$q',
+            '$filter',
             'ngTableParams',
             'eu.crismaproject.pilotE.services.OoI',
             'DEBUG',
-            function ($scope, $q, NgTableParams, ooi, DEBUG) {
+            function ($scope, $q, $filter, NgTableParams, ooi, DEBUG) {
                 'use strict';
-
-                var initTable;
 
                 if (DEBUG) {
                     console.log("initialising master patient directive controller");
@@ -19,75 +18,76 @@ angular.module('eu.crismaproject.pilotE.controllers')
                     throw "IllegalStateException: patients not provided by directive user";
                 }
 
-                initTable = function (allPatients) {
-                    $scope.tableParams = new NgTableParams(
-                        {
-                            page: 1,
-                            count: 10
-                        },
-                        {
-                            total: $scope.patients.length,
-                            getData: function ($defer, params) {
-                                var currentPatients, resolvedPatients, i;
+                $scope.tableParams = new NgTableParams(
+                    {
+                        page: 1,
+                        count: 10,
+                        sorting: { name: 'asc' }
+                    },
+                    {
+                        total: $scope.patients.length,
+                        $scope: {$data: {}},
+                        getData: function ($defer, params) {
+                            var currentPatients, resolvedPatients, i;
 
-                                currentPatients = allPatients.slice(
-                                    (params.page() - 1) * params.count(),
-                                    params.page() * params.count()
-                                );
+                            // FIXME: this won't work with order and filter, we have to apply this paging at the very
+                            // latest, however, shall be fixed as soon as proper backend is available
+                            currentPatients = $scope.patients.slice(
+                                (params.page() - 1) * params.count(),
+                                params.page() * params.count()
+                            );
 
-                                resolvedPatients = [];
+                            resolvedPatients = [];
 
-                                for (i = 0; i < currentPatients.length; ++i) {
-                                    resolvedPatients[i] = ooi.getCapturePatients().get({patientId: currentPatients[i].id}).$promise;
+                            for (i = 0; i < currentPatients.length; ++i) {
+                                // FIXME: we shouldn't load the data here, we either assume that it has already been loaded
+                                // or we load it once and cache it, shall be fixed as soon as proper backend is available
+                                resolvedPatients[i] = ooi.getCapturePatients().get({patientId: currentPatients[i].id}).$promise;
+                            }
+
+                            $q.all(resolvedPatients).then(function (thePatients) {
+                                var patient, i, j, rating, getRatingString, ordered;
+
+                                getRatingString = function (rating) {
+                                    if (rating <= 1.5) {
+                                        return '++';
+                                    } else if (rating <= 2.5) {
+                                        return '+';
+                                    } else if (rating <= 3.5) {
+                                        return '0';
+                                    }
+
+                                    return '';
+                                };
+
+                                for (i = 0; i < thePatients.length; ++i) {
+                                    patient = thePatients[i];
+                                    rating = null;
+                                    for (j = 0; j < patient.careMeasures.length; ++j) {
+                                        rating += patient.careMeasures[j].rating;
+                                    }
+
+                                    if (rating !== null) {
+                                        rating /= patient.careMeasures.length;
+                                        rating = Math.round(rating * 100) / 100;
+                                        patient.averageRating = rating;
+                                        patient.averageRatingString = getRatingString(rating);
+                                    }
                                 }
 
-                                $q.all(resolvedPatients).then(function (thePatients) {
-                                    var patient, i, j, rating, getRatingString;
+                                ordered = params.filter() ? $filter('filter')(thePatients, params.filter()) : thePatients;
+                                ordered = params.sorting() ? $filter('orderBy')(ordered, params.orderBy()) : thePatients;
 
-                                    getRatingString = function (rating) {
-                                        if (rating <= 1.5) {
-                                            return '++';
-                                        } else if (rating <= 2.5) {
-                                            return '+';
-                                        } else if (rating <= 3.5) {
-                                            return '0';
-                                        }
-
-                                        return '';
-                                    };
-
-                                    for (i = 0; i < thePatients.length; ++i) {
-                                        patient = thePatients[i];
-                                        rating = null;
-                                        for (j = 0; j < patient.careMeasures.length; ++j) {
-                                            rating += patient.careMeasures[j].rating;
-                                        }
-
-                                        if (rating !== null) {
-                                            rating /= patient.careMeasures.length;
-                                            rating = Math.round(rating * 100) / 100;
-                                            patient.averageRating = rating;
-                                            patient.averageRatingString = getRatingString(rating);
-                                        }
-                                    }
-                                    $defer.resolve(thePatients);
-                                });
-                            }
+                                params.total(ordered.length);
+                                $defer.resolve(ordered);
+                            });
                         }
-                    );
-                };
-
-                if ($scope.patients.length === 0) {
-                    if ($scope.patients.$promise) {
-                        $scope.patients.$promise.then(function () {
-                            initTable($scope.patients);
-                        });
-                    } else {
-                        throw "IllegalStateException: no patients";
                     }
-                } else {
-                    initTable($scope.patients);
-                }
+                );
+
+                $scope.$watch('patients.length', function (n, o, p) {
+                    $scope.tableParams.reload();
+                });
 
                 $scope.maxCareMeasures = ooi.getMaxCareMeasures();
                 $scope.selectedPatient = null;
