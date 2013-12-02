fd35f0147c206a87beaf724ee6dcd137e8f10d98
Martin Scholl <martin.scholl@cismet.de>
2013-12-02 18:26:50 +0100

    [issue #1] applied ooi service changes to master patient widget

--- app/scripts/controllers/masterPatientDirectiveController.js
+++ app/scripts/controllers/masterPatientDirectiveController.js
@@ -28,58 +28,26 @@ angular.module('eu.crismaproject.pilotE.controllers')
                         total: $scope.patients.length,
                         $scope: {$data: {}},
                         getData: function ($defer, params) {
-                            var currentPatients, resolvedPatients, i;
+                            var resolvedPatients, i;
 
-                            // FIXME: this won't work with order and filter, we have to apply this paging at the very
-                            // latest, however, shall be fixed as soon as proper backend is available
-                            currentPatients = $scope.patients.slice(
-                                (params.page() - 1) * params.count(),
-                                params.page() * params.count()
-                            );
-
+                            // we have to load every patient to ensure proper paging/sorting
                             resolvedPatients = [];
 
-                            for (i = 0; i < currentPatients.length; ++i) {
-                                // FIXME: we shouldn't load the data here, we either assume that it has already been loaded
-                                // or we load it once and cache it, shall be fixed as soon as proper backend is available
-                                resolvedPatients[i] = ooi.getCapturePatients().get({patientId: currentPatients[i].id}).$promise;
+                            for (i = 0; i < $scope.patients.length; ++i) {
+                                resolvedPatients[i] = ooi.getCapturePatients().get({patientId: $scope.patients[i].id}).$promise;
                             }
 
                             $q.all(resolvedPatients).then(function (thePatients) {
-                                var patient, i, j, rating, getRatingString, ordered;
-
-                                getRatingString = function (rating) {
-                                    if (rating <= 1.5) {
-                                        return '++';
-                                    } else if (rating <= 2.5) {
-                                        return '+';
-                                    } else if (rating <= 3.5) {
-                                        return '0';
-                                    }
-
-                                    return '';
-                                };
-
-                                for (i = 0; i < thePatients.length; ++i) {
-                                    patient = thePatients[i];
-                                    rating = null;
-                                    for (j = 0; j < patient.careMeasures.length; ++j) {
-                                        rating += patient.careMeasures[j].rating;
-                                    }
-
-                                    if (rating !== null) {
-                                        rating /= patient.careMeasures.length;
-                                        rating = Math.round(rating * 100) / 100;
-                                        patient.averageRating = rating;
-                                        patient.averageRatingString = getRatingString(rating);
-                                    }
-                                }
+                                var ordered;
 
                                 ordered = params.filter() ? $filter('filter')(thePatients, params.filter()) : thePatients;
                                 ordered = params.sorting() ? $filter('orderBy')(ordered, params.orderBy()) : thePatients;
 
                                 params.total(ordered.length);
-                                $defer.resolve(ordered);
+                                $defer.resolve(ordered.slice(
+                                    (params.page() - 1) * params.count(),
+                                    params.page() * params.count()
+                                ));
                             });
                         }
                     }
